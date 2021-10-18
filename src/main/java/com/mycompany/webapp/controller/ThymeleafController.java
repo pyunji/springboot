571babaf3b484d3f26f448c4d3bb4485c3dc6b08
Pyunji <vusguswl@gmail.com>
2021-10-18 10:16:04 +0900

    1018 중간 커밋

--- src/main/java/com/mycompany/webapp/controller/ThymeleafController.java
+++ src/main/java/com/mycompany/webapp/controller/ThymeleafController.java
@@ -7,14 +7,17 @@ import javax.servlet.http.HttpSession;
 import org.json.JSONObject;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.mycompany.webapp.dto.Board;
 
 import lombok.extern.java.Log;
+import lombok.extern.slf4j.Slf4j;
 
 @Controller
-@Log
+@Slf4j
 @RequestMapping("/thymeleaf")
 public class ThymeleafController {
 	@RequestMapping("/content")
@@ -83,10 +86,31 @@ public class ThymeleafController {
 		model.addAttribute("board", board);
 		return "thymeleaf/selectionVariableExpressions";
 	}
-	
+
 	@RequestMapping("/messageExpressions")
 	public String messageExpressions() {
 		log.info("실행");
 		return "thymeleaf/messageExpressions";
+	}
+
+	// annotation에 여러 값을 줄 때는 중괄호로 묶어준다
+	@RequestMapping({
+		"/linkUrlExpressions/{typeId}/detail",
+		"/linkUrlExpressions/{typeId}/update"
+		})
+	public String linkUrlExpressions(@PathVariable String typeId, @RequestParam(defaultValue = "") String productId,
+			@RequestParam(defaultValue = "1") int pageNo, Model model) {
+		log.info("실행");
+		log.info("typeId: " + typeId);
+		log.info("productId: " + productId);
+		log.info("pageNo: " + pageNo);
+
+		model.addAttribute("typeId", "t1");
+		model.addAttribute("productId", "p1");
+		model.addAttribute("pageNo", "1");
+		model.addAttribute("url1", "/thymeleaf/linkUrlExpressions/t1/detail");
+		model.addAttribute("url2", "/t1/detail");
+
+		return "thymeleaf/linkUrlExpressions";
 	}
 }
